45e2a1f63530c2a2001f110137394665154fd292
Louis Horlaville <louis.horlaville@gmail.com>
2021-02-23 16:34:44 -0500

    fait header et dossiers

--- src/composants/Dossier.jsx
+++ src/composants/Dossier.jsx
@@ -1,15 +1,19 @@
+import SortIcon from '@material-ui/icons/Sort';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import './Dossier.scss'
+
 export default function Dossier({id, titre, couleur, dateModif}) {
   return (
     <article className="Dossier">
       <div className="couverture">
-        <span className="deplacer">Déplacer</span>
+        <span className="deplacer"><SortIcon/></span>
         <img src={`images-dossiers/${id}.png`} alt={titre}/>
       </div>
-      <div className="info">
+      <div className="info" style={{backgroundColor:couleur}}>
         <h2>{titre}</h2>
         <p>{dateModif}</p>
       </div>
-      <button>Modifier</button>
+      <span className="plus"><MoreVertIcon fontSize="default"/></span>
     </article>
   );
 }
--- src/composants/Entete.jsx
+++ src/composants/Entete.jsx
@@ -1,10 +1,12 @@
-import avatar from '../images/avatar.png';
+import { Avatar } from '@material-ui/core';
+import avatarImg from '../images/avatar.png';
+import './Entete.scss';
 
 export default function Entete() {
   return (
     <header className="Entete">
       <div className="logo">Signets</div>
-      <div className="avatar">Camille Semaan <img src={avatar} alt="Camille Semaan" /></div>
+      <div className="avatar">Camille Semaan <Avatar alt="Camille Semaan" src={avatarImg} /></div>
     </header>
   );
 }
